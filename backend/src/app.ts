6e26347c9a959e68156fa660312b41b8d24b2402
icsanabriar <icsanabriar@googlemail.com>
2026-03-17 04:00:05 -0500

    fix: Add cors origin to allow connection in localhost.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
+import cors from 'cors';
 import express from 'express';
 import candidatesRouter from './presentation/routes/candidates';
 import { errorHandler } from './middleware/errorHandler';
 
 export const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 app.use(express.json());
 
 app.get('/', (_req, res) => {
